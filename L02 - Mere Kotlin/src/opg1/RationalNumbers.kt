b24d8da793a2fbde880cf5b8edf081ff324181ea
Lucas <lhasselgaard@gmail.com>
2024-02-14 00:40:43 +0100

    L02 opg1 fremgang

--- L02 - Mere Kotlin/src/opg1/RationalNumbers.kt
+++ L02 - Mere Kotlin/src/opg1/RationalNumbers.kt
@@ -39,27 +39,63 @@ All the following expressions must be evaluated to true:
 • (1 divBy 2) in (1 divBy 3)..(2 divBy 3)
  */
 
+fun main() {
+    println("----------------------------------------------")
+
+    println("(-2 divBy 4).toString() == \"-1/2\"")
+        print((-2 divBy 4).toString() + ", ")
+    println((-2 divBy 4).toString() == "-1/2")
+    println()
+
+    println("(2 divBy 1).toString() == \"2\"")
+        print((2 divBy 1).toString() + ", ")
+    println((2 divBy 1).toString() == "2")
+    println()
+
+    println("\"117/1098\".toRational().toString() == \"13/122\"")
+        print("117/1098".toRational().toString() + ", ")
+    println("117/1098".toRational().toString() == "13/122")
+    println()
+
+}
+
+
+private infix fun Int.divBy(d: Int): Rational {
+    return Rational(n = this, d = d).normalize()
+}
+private fun String.toRational(): Rational {
+    val n: Int
+    val d: Int
+    with(this) {
+        n = substring(0, indexOf('/')).toInt()
+        d = substring(indexOf('/')+1, length).toInt()
+        return Rational(n,d)
+    }
+}
 
 class Rational (var n: Int, var d: Int = 1) : Comparable<Rational> {
-
-    fun toRational(toParse: String) {
-        
+    init {
+        require(d != 0) {"Niks pille!"}
     }
 
-    infix fun divBy(rat: Rational) {
 
+    fun normalize(): Rational {
+        val gcd: Int = normalize(Math.abs(n), Math.abs(d))
+        return Rational(n / gcd * (if (n < 0) -1 else 1), d / gcd)
     }
-
-    fun normalize(rat: Rational) {
-
+    private fun normalize(n: Int, d: Int): Int {
+//        if (d == 0) return n
+//        else return normalize(d, n % d)
+        return if (d == 0) n else normalize(d, n % d)
     }
 
     override fun toString(): String {
-        return "${n}/${d}"
+        val theRat: Rational = this.normalize()
+        if (theRat.d == 1) return "${theRat.n}"
+        else return "${theRat.n}/${theRat.d}"
+        TODO("Skal kunne tage høje for negative tal")
     }
-
     override fun compareTo(other: Rational): Int {
         TODO("Not yet implemented")
     }
-
 }
